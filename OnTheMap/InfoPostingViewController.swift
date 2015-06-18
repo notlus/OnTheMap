e5581fa9c3115b78cbd97d5c78583e9983b4cc4d
Jeffrey Sulton <notlus68@me.com>
2015-06-18 08:42:23 -0700

    Updates to the info posting functionality
    * Hide and show fields based on the posting state
    
    * Save the location found in the postingLocation property
    
    * Call the Parse API to post a new location

--- OnTheMap/InfoPostingViewController.swift
+++ OnTheMap/InfoPostingViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 import MapKit
 
 class InfoPostingViewController: UIViewController {
-
+    private let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    
     @IBOutlet weak var promptLabel: UILabel!
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
@@ -18,12 +19,15 @@ class InfoPostingViewController: UIViewController {
     @IBOutlet weak var urlTextField: UITextField!
     
     private enum PostingState {
+        // Show UI for finding a location
         case FindLocation
         
+        // Show UI for posting a location
         case PostLocation
     }
     
     private var postingState = PostingState.FindLocation
+    private var postLocation: CLLocation?
     
     @IBAction func findOnMap(sender: AnyObject) {
         println("findOnMap")
@@ -51,6 +55,9 @@ class InfoPostingViewController: UIViewController {
                     else {
                         println("Successful local search")
                         if let mapItem = searchResponse.mapItems.first as? MKMapItem {
+                            
+                            self.postLocation = mapItem.placemark.location
+                            
                             self.postingState = PostingState.PostLocation
                             
                             // Show the item on a map
@@ -76,7 +83,34 @@ class InfoPostingViewController: UIViewController {
             if !urlTextField.text.isEmpty, let urlToPost = urlTextField.text {
                 println("url to post: \(urlToPost)")
                 if let url = validateURL(urlToPost) {
+                    println("URL \(urlToPost) is valid!")
                     
+                    // Get the info about the current user
+                    let udacityClient = UdacityClient()
+                    udacityClient.getUserData(appDelegate.userID!, completion: { (userData) -> Void in
+                        if let userData = userData {
+                            println("Got user data: \(userData)")
+                            
+                            var si1 = [String: AnyObject]()
+                            si1["firstName"] = userData["first_name"]
+                            si1["lastName"] = userData["last_name"]
+                            si1["latitude"] = self.postLocation?.coordinate.latitude ?? 0
+                            si1["longitude"] = self.postLocation?.coordinate.longitude ?? 0
+                            si1["mediaURL"] = urlToPost
+                            si1["mapString"] = self.searchField.text
+                            si1["uniqueKey"] = "1234"
+                            
+                            let si = StudentInformation(studentInfo: si1)
+                            
+                            // Post a new location to the Parse API
+                            let parseAPI = StudentLocationClient()
+                            
+                            parseAPI.postStudentLocation(si) { (success) -> Void in
+                                println("Posting complete, success=\(success)")
+                                self.dismissViewControllerAnimated(true, completion: nil)
+                            }
+                        }
+                    })
                 } else {
                     let alert = UIAlertView(title: "Error", message: "Invalid URL", delegate: nil, cancelButtonTitle: "Ok")
                     alert.show()
