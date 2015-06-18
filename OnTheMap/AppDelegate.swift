2026048c00de11c1cf628cf10dedacc9a67bdfa3
Jeffrey Sulton <notlus68@me.com>
2015-06-18 08:31:35 -0700

    Add user ID property

--- OnTheMap/AppDelegate.swift
+++ OnTheMap/AppDelegate.swift
@@ -15,6 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     /// Array of `StudentLocation` objects
     var studentLocations = [StudentInformation]()
+    var userID: String? = nil
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
