4095f54337cb447cb154a9b8cdacdbe45ddfc6e9
Jeffrey Sulton <notlus68@me.com>
2015-06-18 08:37:44 -0700

    Change properties that are not required to be optionals

--- OnTheMap/StudentInformation.swift
+++ OnTheMap/StudentInformation.swift
@@ -10,27 +10,27 @@ import Foundation
 
 /// Represents the location information for a student
 struct StudentInformation {
-    var createdAt: String
+    var createdAt: String?
     var firstName: String
     var lastName: String
     var latitude: Double
     var longitude: Double
     var mapString: String
     var mediaURL: String
-    var objectID: String
-    var uniqueKey: String
-    var updatedAt: String
+    var objectID: String?
+    var uniqueKey: String?
+    var updatedAt: String?
     
     init(studentInfo: [String: AnyObject]) {
-        self.createdAt = studentInfo["createdAt"] as! String
+        self.createdAt = studentInfo["createdAt"] as? String
         self.firstName = studentInfo["firstName"] as! String
         self.lastName = studentInfo["lastName"] as! String
         self.latitude = studentInfo["latitude"] as! Double
         self.longitude = studentInfo["longitude"] as! Double
         self.mapString = studentInfo["mapString"] as! String
         self.mediaURL = studentInfo["mediaURL"] as! String
-        self.objectID = studentInfo["objectId"] as! String
-        self.uniqueKey = studentInfo["uniqueKey"] as! String
-        self.updatedAt = studentInfo["updatedAt"] as! String
+        self.objectID = studentInfo["objectId"] as? String
+        self.uniqueKey = studentInfo["uniqueKey"] as? String
+        self.updatedAt = studentInfo["updatedAt"] as? String
     }
 }
