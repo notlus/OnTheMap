a3a0f9b50dcc7f71fec550a921f3383775679c2d
Jeffrey Sulton <notlus68@me.com>
2015-07-19 19:14:05 -0400

    Work in progress on the information posting view

--- OnTheMap/InfoPostingViewController.swift
+++ OnTheMap/InfoPostingViewController.swift
@@ -9,26 +9,35 @@
 import UIKit
 import MapKit
 
-class InfoPostingViewController: UIViewController {
+class InfoPostingViewController: UIViewController, UITextViewDelegate {
 
     // MARK: Outlets
     @IBOutlet weak var promptLabel: UILabel!
     @IBOutlet weak var searchField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var findOrSubmit: UIButton!
-    @IBOutlet weak var urlTextField: UITextField!
     @IBOutlet weak var activityView: UIActivityIndicatorView!
+    @IBOutlet weak var searchView: UITextView!
+    @IBOutlet weak var questionsView: UIView!
+    @IBOutlet weak var postingView: UIView!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var urlTextView: UITextView!
     
     var mapDelegate: UpdateStudentMap?
     
     private let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-    private enum PostingState {
+    private enum PostingState: Int {
         // Show UI for finding a location
-        case FindLocation
+        case FindLocation = 1
         
         // Show UI for posting a location
-        case PostLocation
+        case PostLocation = 2
     }
+    
+    private let kSearchViewText = "Enter a location"
+    private let kSearchViewTag = 1
+    private let kURLViewText = "Enter a URL to post"
+    private let kURLViewTag = 2
     
     private var postingState = PostingState.FindLocation
     private var postLocation: CLLocation?
@@ -36,25 +45,46 @@ class InfoPostingViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        postingState = PostingState.FindLocation
-        
         // Set the font on the prompt text
         
-        // Normal
-        let thinFont = UIFont(name: "Roboto-Thin", size: 37.0)!
+        // Normal font
+        let thinFont = UIFont(name: "Roboto-Thin", size: 36.0)!
         
-        // Bold
-        let mediumFont = UIFont(name: "Roboto-Medium", size: 37.0)!
-        let promptText: NSMutableAttributedString = promptLabel.attributedText as! NSMutableAttributedString
+        // Bold font
+        let mediumFont = UIFont(name: "Roboto-Medium", size: 36.0)!
+        
+        // Get existing text from the label
+        let promptText =  promptLabel.attributedText as! NSMutableAttributedString
+        
+        // Set the thin font on the entire text
         promptText.addAttribute(NSFontAttributeName, value: thinFont, range: NSRange(location: 0, length: promptText.length))
         
         // Set part of the text to use use the mediume font
         promptText.addAttribute(NSFontAttributeName, value: mediumFont, range: NSRange(location: 13, length: 9))
+        
+        // Set the modified text on the label
         promptLabel.attributedText = promptText
         
-        // Set this class as the delegate for the search text field
-        searchField.delegate = self
-        urlTextField.delegate = self
+        /// Set up the search text view
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.Center
+
+        let searchTextAttributes = NSMutableAttributedString(string: kSearchViewText)
+        let searchRange = NSRange(location: 0, length: searchTextAttributes.length)
+        searchTextAttributes.addAttribute(NSFontAttributeName, value: thinFont, range: searchRange)
+        searchTextAttributes.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: searchRange)
+        searchTextAttributes.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor(), range: searchRange)
+        searchView.attributedText = searchTextAttributes
+        searchView.delegate = self
+        
+        /// Set up the URL text view
+        let urlTextAttributes = NSMutableAttributedString(string: kURLViewText)
+        let urlRange = NSRange(location: 0, length: urlTextAttributes.length)
+        urlTextAttributes.addAttribute(NSFontAttributeName, value: thinFont, range: urlRange)
+        urlTextAttributes.addAttribute(NSParagraphStyleAttributeName, value: paragraphStyle, range: urlRange)
+        urlTextAttributes.addAttribute(NSForegroundColorAttributeName, value: UIColor.blueColor(), range: urlRange)
+        urlTextView.attributedText = urlTextAttributes
+        urlTextView.delegate = self
     }
 
     override func viewWillAppear(animated: Bool) {
@@ -74,111 +104,103 @@ class InfoPostingViewController: UIViewController {
     @IBAction func findOnMap(sender: AnyObject) {
         println("findOnMap")
         
-        if postingState == PostingState.FindLocation {
-            println("PostingState.FindLocation")
-            
-            if !searchField.text.isEmpty, let searchText = searchField.text {
-                // Hide the search field
-                searchField.hidden = true
+        if !searchView.text.isEmpty && searchView.text != kSearchViewText, let searchText = searchView.text {
+            // Hide the search field
+            questionsView.hidden = true
+            activityView.startAnimating()
+            let gc = CLGeocoder()
+            gc.geocodeAddressString(searchText, completionHandler: { (placemarks, geocodeError) -> Void in
+                // Per the documentation, this is always called on the main queue
+                
+                println("Completed search request")
+                
+                // Stop activity view
+                self.activityView.stopAnimating()
+                
+                if let error = geocodeError {
+                    println("Error in search, \(error.description)")
+                    self.questionsView.hidden = false
+                    self.showErrorAlert("Search Error", alertMessage: "Unable to find location")
+                }
+                else if let mapItem = placemarks.first as? CLPlacemark {
+                    println("Successfully geocoded a location")
+                    let placemark = MKPlacemark(placemark: mapItem)
+                    
+                    self.postLocation = mapItem.location
+                    
+                    self.postingState = PostingState.PostLocation
+                    
+                    // Show the item on a map
+                    self.postingView.hidden = false
+                    self.mapView.addAnnotation(placemark)
+                    self.mapView.setCenterCoordinate(mapItem.location.coordinate, animated: true)
+                    self.findOrSubmit.setTitle("Submit", forState: UIControlState.Normal)
+                }
+                else {
+                    // No placemarks
+                    println("No placemarks returned")
+                    self.showErrorAlert("Error", alertMessage: "No placemarks found")
+                }
+            })
+        }
+        else {
+            showErrorAlert("Search", alertMessage: "Please enter an address or location of interest")
+        }
+    }
+    
+    @IBAction func submitURL() {
+        if !urlTextView.text.isEmpty, let urlToPost = NSURL(string: urlTextView.text) {
+            println("url to post: \(urlToPost)")
+            if UIApplication.sharedApplication().canOpenURL(urlToPost) {
+                println("URL \(urlToPost) is valid!")
+                
                 activityView.startAnimating()
-                let gc = CLGeocoder()
-                gc.geocodeAddressString(searchText, completionHandler: { (placemarks, geocodeError) -> Void in
-                    // Per the documentation, this is always called on the main queue
-                    
-                    println("Completed search request")
-                    
-                    // Stop activity view
-                    self.activityView.stopAnimating()
-                    
-                    if let error = geocodeError {
-                        println("Error in search, \(error.description)")
-                        self.searchField.hidden = false
-                        self.showErrorAlert("Search Error", alertMessage: "Unable to find location")
-                    }
-                    else if let mapItem = placemarks.first as? CLPlacemark {
-                        println("Successfully geocoded a location")
-                        let placemark = MKPlacemark(placemark: mapItem)
+                
+                // Get the info about the current user
+                let udacityClient = UdacityClient()
+                udacityClient.getUserData(appDelegate.userID!, completion: { (userData, error) -> Void in
+                    if let userData = userData {
+                        println("Got user data: \(userData)")
                         
-                        self.postLocation = mapItem.location
+                        // Create a dictionary containing the new user data
+                        var studentInfo = [String: AnyObject]()
+                        // TODO: Create constants for the keys
+                        studentInfo["uniqueKey"] = "\(self.appDelegate.userID)"
+                        studentInfo["firstName"] = userData["first_name"]
+                        studentInfo["lastName"] = userData["last_name"]
+                        studentInfo["latitude"] = self.postLocation?.coordinate.latitude ?? 0
+                        studentInfo["longitude"] = self.postLocation?.coordinate.longitude ?? 0
+                        studentInfo["mediaURL"] = urlToPost.absoluteString
+                        studentInfo["mapString"] = self.searchField.text
                         
-                        self.postingState = PostingState.PostLocation
+                        // Post a new location to the Parse API
+                        let parseAPI = StudentLocationClient()
+                        parseAPI.postStudentLocation(studentInfo) { (success, studentInformation) -> Void in
+                            println("Posting complete, success=\(success)")
+                            if let si = studentInformation {
+                                // Call the delegate to update the map
+                                self.mapDelegate?.addToMap(si)
+                            }
+                            
+                            self.dismissViewControllerAnimated(true, completion: nil)
+                        }
+                    } else {
+                        if let error = error {
+                            println("Failed to post student info, error=\(error.description)")
+                        }
                         
-                        // Show the item on a map
-                        self.promptLabel.hidden = true
-                        self.mapView.hidden = false
-                        self.urlTextField.hidden = false
-                        self.mapView.addAnnotation(placemark)
-                        self.mapView.setCenterCoordinate(mapItem.location.coordinate, animated: true)
-                        self.findOrSubmit.setTitle("Submit", forState: UIControlState.Normal)
-                    }
-                    else {
-                        // No placemarks
-                        println("No placemarks returned")
-                        self.showErrorAlert("Error", alertMessage: "No placemarks found")
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.activityView.stopAnimating()
+                            // TODO: Add the ability to retry
+                            self.showErrorAlert("Posting Error", alertMessage: "Unable to post information, please try again")
+                        })
                     }
                 })
-            }
-            else {
-                showErrorAlert("Search", alertMessage: "Please enter an address or location of interest")
-            }
-        } else if postingState == PostingState.PostLocation {
-            println("PostingState.PostLocation")
-            
-            if !urlTextField.text.isEmpty, let urlToPost = NSURL(string: urlTextField.text) {
-                println("url to post: \(urlToPost)")
-                if UIApplication.sharedApplication().canOpenURL(urlToPost) {
-                    println("URL \(urlToPost) is valid!")
-                    
-                    activityView.startAnimating()
-                    
-                    // Get the info about the current user
-                    let udacityClient = UdacityClient()
-                    udacityClient.getUserData(appDelegate.userID!, completion: { (userData, error) -> Void in
-                        if let userData = userData {
-                            println("Got user data: \(userData)")
-
-                            // Create a dictionary containing the new user data
-                            var studentInfo = [String: AnyObject]()
-                            // TODO: Create constants for the keys
-                            studentInfo["uniqueKey"] = "\(self.appDelegate.userID)"
-                            studentInfo["firstName"] = userData["first_name"]
-                            studentInfo["lastName"] = userData["last_name"]
-                            studentInfo["latitude"] = self.postLocation?.coordinate.latitude ?? 0
-                            studentInfo["longitude"] = self.postLocation?.coordinate.longitude ?? 0
-                            studentInfo["mediaURL"] = urlToPost.absoluteString
-                            studentInfo["mapString"] = self.searchField.text
-                            
-                            // Post a new location to the Parse API
-                            let parseAPI = StudentLocationClient()
-                            parseAPI.postStudentLocation(studentInfo) { (success, studentInformation) -> Void in
-                                println("Posting complete, success=\(success)")
-                                if let si = studentInformation {
-                                    // Call the delegate to update the map
-                                    self.mapDelegate?.addToMap(si)
-                                }
-                                
-                                self.dismissViewControllerAnimated(true, completion: nil)
-                            }
-                        } else {
-                            if let error = error {
-                                println("Failed to post student info, error=\(error.description)")
-                            }
-                            
-                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                                self.activityView.stopAnimating()
-                                // TODO: Add the ability to retry
-                                self.showErrorAlert("Posting Error", alertMessage: "Unable to post information, please try again")
-                            })
-                        }
-                    })
-                } else {
-                    showErrorAlert("Invalid URL", alertMessage: "Please enter a valid URL")
-                }
             } else {
-                showErrorAlert("Missing URL", alertMessage: "Please provide a URL")
+                showErrorAlert("Invalid URL", alertMessage: "Please enter a valid URL")
             }
         } else {
-            fatalError("Invalid state!")
+            showErrorAlert("Missing URL", alertMessage: "Please provide a URL")
         }
     }
     
@@ -254,5 +276,17 @@ extension InfoPostingViewController: UITextFieldDelegate {
         textField.resignFirstResponder()
         findOnMap(self)
         return true
+    }
+}
+
+extension InfoPostingViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(textView: UITextView) {
+        textView.text = ""
+    }
+    
+    func textViewDidEndEditing(textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = textView.tag == kSearchViewTag ? kSearchViewText : kURLViewText
+        }
     }
 }
