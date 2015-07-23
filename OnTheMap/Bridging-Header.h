7a390a10cdf0b6740bdc429af7d947a129388548
Jeffrey Sulton <notlus68@me.com>
2015-07-22 22:17:06 -0400

    Add the Facebook SDK to the project

---
+++ OnTheMap/Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Bridging-Header.h
+//  OnTheMap
+//
+//  Created by Jeffrey Sulton on 7/21/15.
+//  Copyright (c) 2015 notlus. All rights reserved.
+//
+
+#ifndef OnTheMap_Bridging_Header_h
+#define OnTheMap_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+#endif
